dfebe1bb29bc29434bef9922c7b208fbc85fa0a3
Aditya Mankare <adityamankare@gmail.com>
2019-08-31 11:09:40 -0500

    Create 1.4-reverseAString.js

---
+++ 1.4-reverseAString.js
@@ -0,0 +1,12 @@
+//Create a function that reversesa string:
+//'Hi, my name is Adi' should be:
+//'idA si eman ym ,iH'
+
+function reverse(str){
+	var splitString = str.split("");
+	var reverseSplitString = splitString.reverse();
+	var result = reverseSplitString.join("")
+	console.log(result);
+}
+
+reverse('Hi, my name is Adi');
