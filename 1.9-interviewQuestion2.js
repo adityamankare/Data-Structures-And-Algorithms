a1fc8d6b348a4504466c21761622dc2b108b8675
Aditya Mankare <adityamankare@gmail.com>
2019-09-03 10:24:51 -0500

    Update 1.9-interviewQuestion2.js

--- 1.9-interviewQuestion2.js
+++ 1.9-interviewQuestion2.js
@@ -8,21 +8,19 @@
 //Given an array = [2,3,4,5]:
 //It should return undefined
 
-
-//BRUTE FORCE APPROACH
 function firstRecurringCharacter(input){
+	let map = {};
 	for(let i = 0; i < input.length; i++){
-		for(let j = i+1; j < input.length; j++){
-			if(input[i]===input[j]){
-				return input[i];
-			}
+		if(map[input[i]] !== undefined){
+			return input[i];
 		}
+		else{
+			map[input[i]] = i;
+		}
+		//console.log(map); //this is what out hash table looks like
 	}
-	return undefined;
-} // O(n^2) not too efficient
+	return undefined 
+}
 
 firstRecurringCharacter([2,5,1,2,3,5,1,2,4]);
-firstRecurringCharacter([2,3,4,5]);
-
-//---
-
+//O(n) 
